fcf2fd85da4f0142dd079df1d60c58fa38a9b5b2
Atharva-Kapare <51484135+Atharva-Kapare@users.noreply.github.com>
2020-09-04 12:20:50 +1000

    Made the profile page and connected to backend

---
+++ vueControllers/profile.js
@@ -0,0 +1,57 @@
+UID = "";
+
+auth.onAuthStateChanged(user =>{
+    if(user){
+        UID = user.uid;
+        console.log("Signed in as: " + user.email);
+
+        var LoggedInUser = db.collection("user").doc(user.uid).get().then(function(doc) {
+
+            if(doc.exists){
+                console.log("Document data:", doc.data());
+                var data = doc.data();
+                profile.firstField = data.first;
+                profile.lastField = data.last;
+                profile.emailField = data.email;
+                profile.addressField = data.address;
+                profile.phoneField = data.phone;
+                profile.client = data.type;
+            } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+
+    });
+        // setUpUI(user);
+    } else{
+        console.log("Not signed in");
+        // setUpUI(user);
+    }
+});
+
+var profile = new Vue({
+    el: '#profile',
+    data: {
+        firstField: "",
+        lastField: "",
+        emailField: "",
+        addressField: "",
+        phoneField: "",
+        client: ""
+    },
+    methods: {
+        updateDetails(){
+            db.collection("user").doc(UID).set({
+                first: this.firstField,
+                last: this.lastField,
+                email: this.emailField,
+                address: this.addressField,
+                phone: this.phoneField,
+                type: this.client
+            })
+            .catch(function(error) {
+                console.error("Error updating document: ", error);
+            });
+        }
+    }
+});
