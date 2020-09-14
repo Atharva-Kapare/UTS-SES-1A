d9d880d8a9f5fed8a2d30efeb411d05695f929ce
himanshujagdishmehta <68589716+himanshujagdishmehta@users.noreply.github.com>
2020-09-14 14:08:47 +1000

    Please check the chat function, i have not finished but do need your help and Atharva help me connect it in back-end

--- assets/js/WebsiteScript.js
+++ assets/js/WebsiteScript.js
@@ -1,6 +1,3 @@
-//JAVASCRIPT FILE FOR THE WHOLE WEBSITE
-
-//changing the top navigation bar into a toggle menu if the window size is too small
 function navbarResize() {
 	var x = document.getElementById("nav");
 	if (x.className === "navigation") {
