11f5f9eaefac5ca6d60be4301b6163b04712bf15
himanshujagdishmehta <68589716+himanshujagdishmehta@users.noreply.github.com>
2020-10-13 21:38:27 +1100

    Deleted everything  in Master

--- assets/js/WebsiteScript.js
+++
@@ -1,9 +0,0 @@
-function navbarResize() {
-	var x = document.getElementById("nav");
-	if (x.className === "navigation") {
-		x.className += " responsive";
-	} else {
-		x.className = "navigation";
-	}
-}
-
